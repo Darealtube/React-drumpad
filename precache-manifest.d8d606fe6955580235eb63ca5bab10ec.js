451c2015e4ee3b1e24a21f26386d857468d6062f
Darealtube <evajavier143@gmail.com>
2020-09-22 08:52:03 +0800

    Updates

---
+++ precache-manifest.d8d606fe6955580235eb63ca5bab10ec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5650866ab93b57f59c4d5f8734c7362c",
+    "url": "/React-drumpad/index.html"
+  },
+  {
+    "revision": "ecc57d7ff9d282f1b673",
+    "url": "/React-drumpad/static/css/main.bcee60dd.chunk.css"
+  },
+  {
+    "revision": "3139138ff7af80c3e371",
+    "url": "/React-drumpad/static/js/2.e129f49e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-drumpad/static/js/2.e129f49e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ad3ab1f494a67f741fd",
+    "url": "/React-drumpad/static/js/3.fe2eadc6.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/React-drumpad/static/js/3.fe2eadc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecc57d7ff9d282f1b673",
+    "url": "/React-drumpad/static/js/main.2b6c2cbb.chunk.js"
+  },
+  {
+    "revision": "5c95438dc9bf253b902e",
+    "url": "/React-drumpad/static/js/runtime-main.b7b533fb.js"
+  },
+  {
+    "revision": "e1c4563cd35825d07f06aaa5030e1ab6",
+    "url": "/React-drumpad/static/media/sfxS.e1c4563c.mp3"
+  }
+]);
